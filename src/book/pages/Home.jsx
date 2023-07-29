ed294f566e61dab369ec68cd8bcb5578a81db12c
goraydev <jofre1236@gmail.com>
2023-07-29 18:47:08 -0500

    Probando con Material Ui

--- src/book/pages/Home.jsx
+++ src/book/pages/Home.jsx
@@ -6,7 +6,7 @@ export const Home = () => {
     <Layout>
       <div className="grid grid-cols-1 md:grid-cols-2 items-center justify-center">
         <h1 className="text-4xl text-center font-semibold">
-          Bienvenido al sistema
+          Bienvenido al sistema (dev)
         </h1>
         <picture className="animation">
           <lottie-player
